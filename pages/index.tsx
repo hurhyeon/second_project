6d07aeae5194f14e6745ce97fde6d6b295acaedb
허현 <nsksjsk2@hanmail.net>
2024-01-03 13:49:28 +0900

    4.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import { NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding:20px;
+  `;
 
 const index: NextPage = () =>{
-  return <div> hello typescript</div>
+  return (
+    <Container>
+      <h1>hello</h1>
+      <h2>hello</h2>
+    </Container>
+  )
 }
 export default index;
