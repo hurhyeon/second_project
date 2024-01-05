23ad2ecfaf6abe651846786501635efdd663f471
허현 <nsksjsk2@hanmail.net>
2024-01-05 22:08:47 +0900

    6.6

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { NextPage } from "next";
-import TodoList from "../components/TodoList"
-// import { TodoType } from "../types/todo";
+import TodoList from "../components/TodoList";
+import { getTodosAPI } from "../lib/api/todo";
+import { wrapper } from "../store";
+import { todoActions } from "../store/todo";
 
-// interface IProps{
-//   todos:TodoType[];
-// }
-// const TodoList: React.FC<IProps> = () => {
-const todos: TodoType[]=[
-  {id:1, text:"마트 가서 장보기", color:"red", checked:false},
-  {id:2, text:"숙제하기", color:"orange", checked:false},
-  {id:3, text:"놀기", color:"yellow", checked:false},
-  {id:4, text:"먹기", color:"green", checked:true},
-]
-
-const app: NextPage = () =>{
-  return <TodoList todos={todos} />;
+const app: NextPage = () => {
+  return <TodoList />;
 };
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store }) => {
+    try {
+      const { data } = await getTodosAPI();
+      store.dispatch(todoActions.setTodo(data));
+      return { props: {} };
+    } catch (e) {
+      console.log(e);
+      return { props: {} };
+    }
+  }
+);
 
 export default app;
